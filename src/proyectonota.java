b3d81e0edc0ac77fa2fc8c57b2853e7805f52098
IsaacGlezPal <izak963gonzalez@gmail.com>
2024-12-05 14:48:39 -0500

    Agregando documentación generada por Javadoc

--- src/proyectonota.java
+++ src/proyectonota.java
@@ -1,34 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-class Estudiante {
-    private int id;
-    private String nombre;
-    private List<Integer> listaCalificaciones;
+public class proyectoNotas() {
 
-    public Estudiante(int id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-        this.listaCalificaciones = new ArrayList<>();
-    }
 
-    public void agregarCalificacion(int calificacion) {
-        if (calificacion >= 0 && calificacion <= 100) {
-            listaCalificaciones.add(calificacion);
-        } else {
-            System.out.println("Calificación no válida. Debe estar entre 0 y 100.");
+    class Estudiante {
+        private int id;
+        private String nombre;
+        private List<Integer> listaCalificaciones;
+
+        public Estudiante(int id, String nombre) {
+            this.id = id;
+            this.nombre = nombre;
+            this.listaCalificaciones = new ArrayList<>();
         }
-    }
 
-    public double calcularPromedio() {
-        if (listaCalificaciones.isEmpty()) {
-            return 0;
+        public void agregarCalificacion(int calificacion) {
+            if (calificacion >= 0 && calificacion <= 100) {
+                listaCalificaciones.add(calificacion);
+            } else {
+                System.out.println("Calificación no válida. Debe estar entre 0 y 100.");
+            }
         }
-        int sum = 0;
-        for (int calificacion : listaCalificaciones) {
-            sum += calificacion;
+
+        public double calcularPromedio() {
+            if (listaCalificaciones.isEmpty()) {
+                return 0;
+            }
+            int sum = 0;
+            for (int calificacion : listaCalificaciones) {
+                sum += calificacion;
+            }
+            return (double) sum / listaCalificaciones.size();
         }
-        return (double) sum / listaCalificaciones.size();
     }
 }
